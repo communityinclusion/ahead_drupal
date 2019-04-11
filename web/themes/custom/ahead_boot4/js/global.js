ddb8b10ad7c58877a3699d15a1cc01dba3fc51ad
Paul Foos <paul.foos@umb.edu>
2019-04-11 12:44:15 -0400

    first commit

---
+++ web/themes/custom/ahead_boot4/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ahead_boot4 = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
