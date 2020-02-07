fe89165de83a2505bab3438504e24f6d838d3fdc
Paul Foos <paul.foos@umb.edu>
2020-02-07 15:06:00 -0500

    js fixes for sidebars

--- web/themes/custom/ahead_boot4/js/global.js
+++ web/themes/custom/ahead_boot4/js/global.js
@@ -10,12 +10,6 @@
   Drupal.behaviors.ahead_boot4 = {
     attach: function (context, settings) {
 
-      
-      $( "#block-institutionsize",context ).once('block-institutionsize').addClass('hideAdv');
-      $( "#block-disabilityrelatedprogramsandcourses",context ).once('block-disabilityrelatedprogramsandcourses').addClass('hideAdv');
-      $( "#block-disabilityrelatedservices",context ).once('block-disabilityrelatedservices').addClass('hideAdv');
-      $( "#block-minorityserving",context ).once('block-minorityserving').addClass('hideAdv');
-      $( "#block-coursesordegreeprogramsintheareaofdisability",context ).once('block-coursesordegreeprogramsintheareaofdisability').addClass('hideAdv');
       $( "#advButton",context ).once('advButton').click(function() {
         
         $('#block-institutionsize').toggle( "slow", function() {
@@ -33,10 +27,7 @@
         $('#block-coursesordegreeprogramsintheareaofdisability').toggle( "slow", function() {
           // Animation complete.
         });
-      }).click();
-        $('.view-header').hide();
-        $('.block-publicorprivate',context).once('block-publicorprivate').each(function() { $('<p id="advButton">Test</p>').insertBefore(this);
-        });
+      });
       
         /* $(context).find('#sidebar_first').once('#advButton').each( function() {
 
