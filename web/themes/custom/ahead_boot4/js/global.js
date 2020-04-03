4a6299622486911b60912fd4ea76a6cf878682e1
Paul Foos <paul.foos@umb.edu>
2020-04-03 14:22:56 -0400

    template changes

--- web/themes/custom/ahead_boot4/js/global.js
+++ web/themes/custom/ahead_boot4/js/global.js
@@ -36,14 +36,14 @@
         
         }
         );
-        
+
         $('legend.legendDescrip',context).once('initHidden').each( function() {
           $(this).append('<p class="ratingKey">Rating key:</p><ul class="tableKey clearfix"><li><img src="/sites/default/files/cedar_imgs/commonly_provided.png" style="width:18px" alt="Commonly provided in the last three years" />&nbsp;&nbsp;Commonly provided in the last three years</li><li><img src="/sites/default/files/cedar_imgs/occasionally_provided.png" style="width:18px" alt="Occasionally provided in the last three years" />&nbsp;&nbsp;Occasionally provided in the last three years</li><li><img src="/sites/default/files/cedar_imgs/not_provided.png" style="width:18px" alt="Not provided in the last three years" />&nbsp;&nbsp;Not provided in the last three years</li></ul>');
 
         });
         
 
-        $('div.campusAcc',context).once('initHidden').each( function() {
+        $('div#campusacc',context).once('initHidden').each( function() {
           $(this).prepend('<p class="ratingKey">Rating key:</p><ul class="tableKey clearfix"><li><img src="/sites/default/files/cedar_imgs/commonly_provided.png" style="width:18px" alt="Completely accessible" />&nbsp;&nbsp;Completely accessible</li><li><img src="/sites/default/files/cedar_imgs/occasionally_provided.png" style="width:18px" alt="Somewhat accessible" />&nbsp;&nbsp;Somewhat accessible</li><li><img src="/sites/default/files/cedar_imgs/not_provided.png" style="width:18px" alt="Generally not accessible" />&nbsp;&nbsp;Generally not accessible</li></ul>');
 
         });      
