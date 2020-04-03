7cd6ec9d25ef8acbd337d8160e73e4f78b6b588c
Paul Foos <paul.foos@umb.edu>
2020-04-03 14:30:39 -0400

    template changes

--- web/themes/custom/ahead_boot4/js/global.js
+++ web/themes/custom/ahead_boot4/js/global.js
@@ -43,8 +43,8 @@
         });
         
 
-        $('div#campusacc',context).once('initHidden').each( function() {
-          $(this).append('<p class="ratingKey">Rating key:</p><ul class="tableKey clearfix"><li><img src="/sites/default/files/cedar_imgs/commonly_provided.png" style="width:18px" alt="Completely accessible" />&nbsp;&nbsp;Completely accessible</li><li><img src="/sites/default/files/cedar_imgs/occasionally_provided.png" style="width:18px" alt="Somewhat accessible" />&nbsp;&nbsp;Somewhat accessible</li><li><img src="/sites/default/files/cedar_imgs/not_provided.png" style="width:18px" alt="Generally not accessible" />&nbsp;&nbsp;Generally not accessible</li></ul>');
+        $('div#campusAcc',context).once('initHidden').each( function() {
+          $(this).prepend('<p class="ratingKey">Rating key:</p><ul class="tableKey clearfix"><li><img src="/sites/default/files/cedar_imgs/commonly_provided.png" style="width:18px" alt="Completely accessible" />&nbsp;&nbsp;Completely accessible</li><li><img src="/sites/default/files/cedar_imgs/occasionally_provided.png" style="width:18px" alt="Somewhat accessible" />&nbsp;&nbsp;Somewhat accessible</li><li><img src="/sites/default/files/cedar_imgs/not_provided.png" style="width:18px" alt="Generally not accessible" />&nbsp;&nbsp;Generally not accessible</li></ul>');
 
         });      
      
