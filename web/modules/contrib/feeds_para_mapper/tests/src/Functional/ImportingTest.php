0d9fe55233371ff203c224e2880465a3f66179fa
Paul Foos <paul.foos@umb.edu>
2021-11-05 16:08:28 -0400

    moving modules aroudn

---
+++ web/modules/contrib/feeds_para_mapper/tests/src/Functional/ImportingTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\Tests\feeds_para_mapper\Functional;
+
+
+
+/**
+ * Test Importing.
+ * @group Feeds Paragraphs
+ */
+class ImportingTest extends FeedsParaMapperTestBase {
+  protected $defaultTheme = 'stark';
+  protected function setUp()
+  {
+    parent::setUp();
+  }
+
+  public function testThings(){
+    $this->drupalGet('http://localhost/admin/structure/feeds');
+    debug('my data');
+  }
+}
